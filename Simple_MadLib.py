22a71bbcf56cb8ad14f65d8b6db0c668347fe100
Colby Canselo <105254002+ColbyC1@users.noreply.github.com>
2022-08-18 14:43:23 -0500

    Create Simple_MadLib.py

---
+++ Simple_MadLib.py
@@ -0,0 +1,12 @@
+def MadLib():
+    
+    print('Madlibs you say?')
+    
+    color = input('Choose a color: ')
+    noun = input('Next, Choose a noun: ')
+    celebrity = input('Lastly, Choose a celebrity: ')
+    print('\n')
+    
+    print(f'Poem: \nRoses are {color} \n{noun} are blue \nI love {celebrity}')
+
+# MadLib()
